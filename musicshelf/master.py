b7dc2a065c81d4abaa39c221a89164d69e329b27
uninformed <iguaranteenoonehasthisusername@gmail.com>
2018-07-13 12:53:25 -0500

    Add new master view

--- musicshelf/master.py
+++ musicshelf/master.py
@@ -7,13 +7,6 @@ from musicshelf.db import get_db
 
 bp = Blueprint('master', __name__, url_prefix='/master')
 
-@bp.route('/new')
-@login_required
-def new():
-
-
-
-    return render_template('master/new.html')
 
 @bp.route('/<int:id>')
 def masterdetail(id):
@@ -90,3 +83,51 @@ def masteredit(id):
     )
     master = cursor.fetchone()
     return render_template('master/masteredit.html', master=master)
+
+@bp.route('/new', methods=('GET', 'POST'))
+@login_required
+def new():
+    """
+    Add new master entry.
+    """
+    cursor = get_db().cursor()
+    # check whether we're processing the submitted form
+    if request.method == 'POST':
+        # gather variables
+        title = request.form['title']
+        artist = request.form['artist']
+        cert = request.form['cert']
+        year = request.form['year']
+        genres = request.form['genres']
+        producer = request.form['producer']
+        cover_designer = request.form['cover_designer']
+        discogs_mid = request.form['discogs_mid']
+        comments = request.form['comments']
+
+        # attempt to filter out <script> tags
+        comments = re.sub('<(\/?)script', '<\\1code', comments)
+
+        error = None
+
+        # a little validation
+        if cert not in ['none', 'gold', 'platinum', 'multi-platinum', 'diamond']:
+            error = 'Invalid certification.'
+        elif int(year) < 1900:
+            error = 'Invalid year.'
+        #TODO think of more, maybe?
+
+        if error is None:
+            ret = cursor.execute(
+                'INSERT INTO msmaster'
+                ' (title, artist, cert, year, genres, producer, cover_designer, discogs_mid, comments)'
+                ' VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                (title, artist, cert, year, genres, producer, cover_designer, discogs_mid, comments)
+            )
+            cursor.execute('SELECT LAST_INSERT_ID() AS id')
+            res = cursor.fetchone()
+
+            return redirect(url_for('master.masterdetail', id=res['id']))
+        flash(error)
+
+
+    return render_template('master/new.html')
